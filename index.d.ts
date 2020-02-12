b7dc0f52701454936069bad2ed3c0ad548c9fa93
bakatrouble <bakatrouble@gmail.com>
2020-02-12 13:37:05 +0300

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+import { MongoClient } from 'mongodb';
+import { UpdateWriteOpResult, Db, DbCollectionOptions } from 'mongodb';
+import { Telegraf, ContextMessageUpdate } from 'telegraf';
+
+declare module 'telegraf-session-mongodb' {
+  class TelegrafMongoSession {
+    constructor(db: Db, options: DbCollectionOptions);
+    saveSession(key: string, session: any): Promise<UpdateWriteOpResult>;
+    getSession(key: string): Promise<any>;
+    getSessionKey(ctx: ContextMessageUpdate): string;
+    middleware(ctx: ContextMessageUpdate, next: any): void;
+    static setup(bot: Telegraf<ContextMessageUpdate>, mongo_url: string, params: DbCollectionOptions): Promise<MongoClient>;
+  }
+
+  export function middleware(db: Db, options: DbCollectionOptions): (ctx: ContextMessageUpdate, next: any) => void;
+}
