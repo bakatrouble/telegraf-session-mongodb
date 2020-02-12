3976c603c367da5d30313307a02e8774035fe324
bakatrouble <bakatrouble@gmail.com>
2020-02-12 13:47:38 +0300

    Fix options type

--- index.d.ts
+++ index.d.ts
@@ -3,14 +3,19 @@ import { UpdateWriteOpResult, Db, DbCollectionOptions } from 'mongodb';
 import { Telegraf, ContextMessageUpdate } from 'telegraf';
 
 declare module 'telegraf-session-mongodb' {
+  interface TelegrafSessionMongoOptions extends DbCollectionOptions {
+    collectionName: string;
+    sessionName: string;
+  }
+  
   class TelegrafMongoSession {
-    constructor(db: Db, options: DbCollectionOptions);
+    constructor(db: Db, options: TelegrafSessionMongoOptions);
     saveSession(key: string, session: any): Promise<UpdateWriteOpResult>;
     getSession(key: string): Promise<any>;
     getSessionKey(ctx: ContextMessageUpdate): string;
     middleware(ctx: ContextMessageUpdate, next: any): void;
-    static setup(bot: Telegraf<ContextMessageUpdate>, mongo_url: string, params?: DbCollectionOptions): Promise<MongoClient>;
+    static setup(bot: Telegraf<ContextMessageUpdate>, mongo_url: string, params?: TelegrafSessionMongoOptions): Promise<MongoClient>;
   }
 
-  export function middleware(db: Db, options?: DbCollectionOptions): (ctx: ContextMessageUpdate, next: any) => void;
+  export function middleware(db: Db, options?: TelegrafSessionMongoOptions): (ctx: ContextMessageUpdate, next: any) => void;
 }
