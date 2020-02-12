48b9bb0df467fc29ad2f9d3ba1ef55f0412d4b85
bakatrouble <bakatrouble@gmail.com>
2020-02-12 13:39:53 +0300

    Fix typings

--- index.d.ts
+++ index.d.ts
@@ -9,8 +9,8 @@ declare module 'telegraf-session-mongodb' {
     getSession(key: string): Promise<any>;
     getSessionKey(ctx: ContextMessageUpdate): string;
     middleware(ctx: ContextMessageUpdate, next: any): void;
-    static setup(bot: Telegraf<ContextMessageUpdate>, mongo_url: string, params: DbCollectionOptions): Promise<MongoClient>;
+    static setup(bot: Telegraf<ContextMessageUpdate>, mongo_url: string, params?: DbCollectionOptions): Promise<MongoClient>;
   }
 
-  export function middleware(db: Db, options: DbCollectionOptions): (ctx: ContextMessageUpdate, next: any) => void;
+  export function middleware(db: Db, options?: DbCollectionOptions): (ctx: ContextMessageUpdate, next: any) => void;
 }
